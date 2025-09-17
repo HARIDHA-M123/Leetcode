0994f4a74c1f7358d594ef5de653a465218b9026
HARIDHA-M123 <haridhamadhesh@gmail.com>
2025-09-17 15:47:05 +0530

    Time: 7 ms (20.28%) | Memory: 9.6 MB (91.18%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int> mp;
+        for(int i=0;i<s.size();i++)
+        mp[s[i]]++;
+        for(int i=0;i<t.size();i++)
+        {
+            mp[t[i]]--;
+            if(mp[t[i]]==0)
+              mp.erase(t[i]);
+        }
+        return mp.empty();
+        }
+    
+};
