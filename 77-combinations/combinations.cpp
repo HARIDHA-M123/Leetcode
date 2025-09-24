2795099407f4fad7ac15d748ad99508e75060d31
HARIDHA-M123 <haridhamadhesh@gmail.com>
2025-09-24 11:07:26 +0530

    Time: 38 ms (65.07%) | Memory: 62.4 MB (82.38%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void backtrack(int n,int k,vector<vector<int>> &res,vector<int> &combi,int curr){
+        if(combi.size()==k){
+            res.push_back(combi);
+            return;
+        }
+        for(int i=curr;i<=n;i++){
+            combi.push_back(i);
+            backtrack(n,k,res,combi,i+1);
+            combi.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n,int k){
+        vector<vector<int>> res;
+        vector<int> combi;
+        backtrack(n,k,res,combi,1);
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
+        
